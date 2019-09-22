6ae594d47526c49b0f9861f2fcb7d9b8c211198b
Hafidz Prasetya <apitgelo@gmail.com>
2019-09-22 23:24:17 +0700

    Set global setting of django-rest

--- config/settings.py
+++ config/settings.py
@@ -128,3 +128,13 @@ try:
     from local_settings import *
 except ImportError as e:
     pass
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        # 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10
+}
