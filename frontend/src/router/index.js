a894914ad5dd50610560d4337c9023361b2d2ef8
Hafidz Prasetya <apitgelo@gmail.com>
2019-09-22 21:45:58 +0700

    Include

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Chat from '@/components/Chat'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/chats',
+      name: 'Chat',
+      component: Chat
     }
   ]
 })
+
+export default router
