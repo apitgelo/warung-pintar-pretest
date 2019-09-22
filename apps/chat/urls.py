1e61a48689fd56d8163aec1ac48ae6bb038d284e
Hafidz Prasetya <apitgelo@gmail.com>
2019-09-22 23:23:50 +0700

    Define route for getting list of messages

--- apps/chat/urls.py
+++ apps/chat/urls.py
@@ -1,1 +1,9 @@
 from django.conf.urls import url, include
+
+from .views import message
+
+urlpatterns = [
+    url(r'^chat/', include([
+        url(r'^$', message.MessageViewSet.as_view({"get": "list"}), name="meeting-list"),
+    ]))
+]
